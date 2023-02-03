f5e7e1ac9400e6442820ea1093b7e1506eb7f761
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-02-03 00:38:03 -0600

    testing gui

---
+++ index.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+import win32clipboard
+
+root = Tk()
+frm  = ttk.Frame(root, padding=20)
+frm.grid()
+ttk.Label(frm, text="System Clipboard Notifications Settings").grid(column=0, row=0)
+
+def getClipboard():
+    win32clipboard.OpenClipboard()
+    clipboardText = win32clipboard.GetClipboardData()
+    ttk.Label(frm, text="pre-render").grid(column=0, row=2)    
+    ttk.Label(frm, text=clipboardText).grid(column=0, row=3)
+    ttk.Label(frm, text="render").grid(column=0, row=4)
+getClipboard()
+root.mainloop()
