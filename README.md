1e6734d91c94255a9e20d388c60093db4ba1779c
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-02-05 23:16:49 -0600

    working sound

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # system-clipboard-notifications-windows
+
+## Credits
+- 
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import win32clipboard
+from playsound import playsound
 
 root = Tk()
 frm  = ttk.Frame(root, padding=20)
@@ -10,8 +11,10 @@ ttk.Label(frm, text="System Clipboard Notifications Settings").grid(column=0, ro
 def getClipboard():
     win32clipboard.OpenClipboard()
     clipboardText = win32clipboard.GetClipboardData()
+    
     ttk.Label(frm, text="pre-render").grid(column=0, row=2)    
     ttk.Label(frm, text=clipboardText).grid(column=0, row=3)
     ttk.Label(frm, text="render").grid(column=0, row=4)
+    playsound('audio/copied.mp3')
 getClipboard()
 root.mainloop()
