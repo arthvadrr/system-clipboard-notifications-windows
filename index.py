e7db2280b5bc049e3bb25efb495382585e7a9daf
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-02-05 23:44:27 -0600

    listener working for copy

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import time
 import win32clipboard
 from playsound import playsound
 
@@ -9,12 +10,24 @@ frm.grid()
 ttk.Label(frm, text="System Clipboard Notifications Settings").grid(column=0, row=0)
 
 def getClipboard():
-    win32clipboard.OpenClipboard()
-    clipboardText = win32clipboard.GetClipboardData()
-    
     ttk.Label(frm, text="pre-render").grid(column=0, row=2)    
-    ttk.Label(frm, text=clipboardText).grid(column=0, row=3)
     ttk.Label(frm, text="render").grid(column=0, row=4)
-    playsound('audio/copied.mp3')
+    listener()
+
+def listener():
+    listening = True
+    cached_clipboard_text = ''
+
+    while listening:
+        win32clipboard.OpenClipboard()
+        current_clipboard_text = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+
+        if current_clipboard_text != cached_clipboard_text:
+            cached_clipboard_text = current_clipboard_text
+            playsound('audio/copied.mp3')
+            print('copied')
+        time.sleep(0.5)
+
 getClipboard()
 root.mainloop()
