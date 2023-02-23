172b6ec748b69f117f277c76884088bb323c31fe
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-02-23 00:46:28 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,26 @@
-from tkinter import *
-from tkinter import ttk
-import time
+from   tkinter   import *
+from   tkinter   import ttk
+from   playsound import playsound
+from   PIL import Image, ImageDraw
 import win32clipboard
-from playsound import playsound
+import pystray
+import time
 
-root = Tk()
-frm  = ttk.Frame(root, padding=20)
-frm.grid()
-ttk.Label(frm, text="System Clipboard Notifications Settings").grid(column=0, row=0)
+# root = Tk()
+# root.protocol("WM_DELETE_WINDOW", root.iconify)
+# root.mainloop()
 
-def getClipboard():
-    ttk.Label(frm, text="pre-render").grid(column=0, row=2)    
-    ttk.Label(frm, text="render").grid(column=0, row=4)
-    listener()
+# frm  = ttk.Frame(root, padding=20)
+# frm.grid()
+# ttk.Label(frm, text="System Clipboard Notifications Settings").grid(column=0, row=0)
 
-def listener():
+def scnw_init():
+    # ttk.Label(frm, text="pre-render").grid(column=0, row=2)    
+    # ttk.Label(frm, text="render").grid(column=0, row=4)
+    scnw_systray()
+    scnw_listener()
+
+def scnw_listener():
     listening = True
     cached_clipboard_text = ''
 
@@ -26,8 +32,17 @@ def listener():
         if current_clipboard_text != cached_clipboard_text:
             cached_clipboard_text = current_clipboard_text
             playsound('audio/copied.mp3')
-            print('copied')
         time.sleep(0.5)
 
-getClipboard()
-root.mainloop()
+def create_image(width, height, color1, color2):
+    image = Image.new('RGB', (width, height), color1)
+    drawn = ImageDraw.Draw(image)
+    drawn.rectangle((width // 2, 0, width, height // 2), fill=color2)
+    drawn.rectangle((0, height // 2, width // 2, height), fill=color2)
+    return image
+
+def scnw_systray():
+    icon = pystray.Icon('Clipboard Notifications Settings', icon=create_image(64, 64, 'black', 'white'))
+    icon.run()
+
+scnw_init()
